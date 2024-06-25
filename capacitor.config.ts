3b8feba368a6238c3412ad55614d7c4cbdea441d
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-06-25 16:13:12 +0400

    add android support

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.otaku.app',
+  appName: 'Ota-ku',
+  webDir: 'dist'
+};
+
+export default config;
