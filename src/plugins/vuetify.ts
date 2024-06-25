18870c6c72e00d1c148fd00acfda97243b12b8df
Vahe Sargsyan <140258231+w33bvGL@users.noreply.github.com>
2024-06-25 15:49:34 +0400

    vuetify.ts configs

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,5 +5,28 @@ import "vuetify/styles";
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
+const customDarkOne= {
+    dark: true,
+    colors: {
+        background: '#ffffff',
+        surface: '#1763b1',
+        primary: '#0047ff',
+        'primary-darken-1': '#afa5df',
+        secondary: '#212121',
+        'secondary-darken-1': '#018786',
+        error: '#0337bc',
+        info: '#2196F3',
+        success: '#4CAF50',
+        warning: '#FB8C00',
+    },
+}
+
+export default createVuetify({
+    theme: {
+        defaultTheme: 'dark',
+        themes: {
+            customDarkOne,
+        }
+    }
+})
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
